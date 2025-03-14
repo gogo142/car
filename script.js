92a9af8b26530e7ddfb64a47d6c047b4337aa71d
George Manaz <george@MacBook-Air-som-tillhor-George.local>
2025-03-14 23:06:42 +0100

    fixed img slider

--- script.js
+++ script.js
@@ -3,13 +3,6 @@ const swiper = new Swiper('.slider-wrapper', {
     grabCursor: true,
     spaceBetween: 30,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true
-    },
-  
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
